ce413e9848ad4693a3f6309278383149900f5d5c
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2022-09-24 03:00:13 +0600

    js

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// toggle for menu 
+
+const toggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
+
+
+//scroll vanishing menu
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.querySelector(".showcase.active").style.right = "0";
+//   } 
+//   else {
+//     document.querySelector(".showcase.active").style.right = "300px";
+//   }
+// }
+
+
+//slide for team member
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+    var w = document.documentElement.clientWidth || window.innerWidth;
+
+    if(w > 568){
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        if (n > slides.length){
+            slideIndex = 1
+            }    
+        if (n < 1){
+            slideIndex = slides.length
+            }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slides[slideIndex-1].style.display = "flex";  
+    }else if(w < 569){
+        let i;
+        let slides = document.getElementsByClassName("mobSlides");
+        if (n > slides.length){
+            slideIndex = 1
+            }    
+        if (n < 1){
+            slideIndex = slides.length
+            }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slides[slideIndex-1].style.display = "flex";  
+    }
+}
